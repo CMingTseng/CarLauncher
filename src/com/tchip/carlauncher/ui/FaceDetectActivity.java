376d185e863c25e000ee12c6be11b97f61757d59
zhoumushui <zhoumushui@gmail.com>
2015-05-23 12:02:28 +0800

    [ZMS]Optimize layout of FaceDetectActivity.

--- src/com/tchip/carlauncher/ui/FaceDetectActivity.java
+++ src/com/tchip/carlauncher/ui/FaceDetectActivity.java
@@ -28,6 +28,7 @@ import android.view.WindowManager;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.facepp.error.FaceppParseException;
@@ -45,9 +46,12 @@ public class FaceDetectActivity extends Activity {
 
 	private ImageView imagePhoto = null;
 	private Bitmap bitmapPhoto = null;
-	private ButtonFloat buttonDetect, btnShare;
-	private TextView textState = null;
+	private ButtonFloat btnDetect, btnShare;
+	private TextView textState, textAge;
 	private View frameWait;
+	private TextView textHint;
+	private ImageView imageHintArrow1, imageHintArrow2, imageHintArrow3;
+	private ProgressBar detectProgress;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -58,14 +62,13 @@ public class FaceDetectActivity extends Activity {
 
 		setContentView(R.layout.activity_face_detect);
 
-		ButtonFloat btnGetImage = (ButtonFloat) this
-				.findViewById(R.id.btnGetImage);
+		ButtonFloat btnGetImage = (ButtonFloat) findViewById(R.id.btnGetImage);
 		btnGetImage.setDrawableIcon(getResources().getDrawable(
 				R.drawable.icon_face_add_image));
 		btnGetImage.hasAnimation(false);
 		btnGetImage.setOnClickListener(new MyOnClickListener());
 
-		btnShare = (ButtonFloat) this.findViewById(R.id.btnShare);
+		btnShare = (ButtonFloat) findViewById(R.id.btnShare);
 		btnShare.setDrawableIcon(getResources().getDrawable(
 				R.drawable.icon_face_share));
 		btnShare.hasAnimation(false);
@@ -73,17 +76,28 @@ public class FaceDetectActivity extends Activity {
 
 		frameWait = findViewById(R.id.frameWait);
 
-		textState = (TextView) this.findViewById(R.id.textState);
+		textState = (TextView) findViewById(R.id.textState);
 
-		buttonDetect = (ButtonFloat) this.findViewById(R.id.buttonDetect);
-		buttonDetect.setVisibility(View.INVISIBLE);
-		buttonDetect.setDrawableIcon(getResources().getDrawable(
+		btnDetect = (ButtonFloat) findViewById(R.id.btnDetect);
+		btnDetect.setVisibility(View.INVISIBLE);
+		btnDetect.setDrawableIcon(getResources().getDrawable(
 				R.drawable.icon_face_smile));
-		buttonDetect.hasAnimation(false);
-		buttonDetect.setOnClickListener(new MyOnClickListener());
+		btnDetect.hasAnimation(false);
+		btnDetect.setOnClickListener(new MyOnClickListener());
 
-		imagePhoto = (ImageView) this.findViewById(R.id.imagePhoto);
+		imagePhoto = (ImageView) findViewById(R.id.imagePhoto);
 		imagePhoto.setImageBitmap(bitmapPhoto);
+
+		textHint = (TextView) findViewById(R.id.textHint);
+		imageHintArrow1 = (ImageView) findViewById(R.id.imageHintArrow1);
+		imageHintArrow2 = (ImageView) findViewById(R.id.imageHintArrow2);
+		imageHintArrow3 = (ImageView) findViewById(R.id.imageHintArrow3);
+		imageHintArrow1.setVisibility(View.VISIBLE);
+		imageHintArrow2.setVisibility(View.INVISIBLE);
+		imageHintArrow3.setVisibility(View.INVISIBLE);
+
+		textAge = (TextView) frameWait.findViewById(R.id.textAge);
+		detectProgress = (ProgressBar) findViewById(R.id.detectProgress);
 	}
 
 	class MyOnClickListener implements View.OnClickListener {
@@ -97,8 +111,10 @@ public class FaceDetectActivity extends Activity {
 				photoPickerIntent.setType("image/*");
 				startActivityForResult(photoPickerIntent, PICTURE_CHOOSE);
 				break;
-			case R.id.buttonDetect:
-				frameWait.setVisibility(View.VISIBLE);
+			case R.id.btnDetect:
+				// frameWait.setVisibility(View.VISIBLE);
+				detectProgress.setVisibility(View.VISIBLE);
+				// textAge.setVisibility(View.INVISIBLE);
 				FaceDetectUtil.detect(bitmapPhoto,
 						new FaceDetectUtil.FaceCallBack() {
 
@@ -132,16 +148,15 @@ public class FaceDetectActivity extends Activity {
 		public void handleMessage(android.os.Message msg) {
 			switch (msg.what) {
 			case MSG_SUCCESS:
-				frameWait.setVisibility(View.GONE);
+				// frameWait.setVisibility(View.GONE);
 
 				JSONObject rst = (JSONObject) msg.obj;
 				Log.e(TAG, rst.toString());
 				prepareBitmap(rst);
 				imagePhoto.setImageBitmap(bitmapPhoto);
-				btnShare.setVisibility(View.VISIBLE);
 				break;
 			case MSG_ERROR:
-				frameWait.setVisibility(View.GONE);
+				// frameWait.setVisibility(View.GONE);
 				String errorMsg = (String) msg.obj;
 				if (TextUtils.isEmpty(errorMsg)) {
 					textState.setText("Error");
@@ -157,8 +172,6 @@ public class FaceDetectActivity extends Activity {
 	};
 
 	private Bitmap getGendorBitmap(int age, boolean isMale) {
-		// frameWait.setVisibility(View.VISIBLE);
-		TextView textAge = (TextView) frameWait.findViewById(R.id.textAge);
 		textAge.setVisibility(View.VISIBLE);
 		textAge.setText("" + age);
 		if (isMale) {
@@ -173,6 +186,7 @@ public class FaceDetectActivity extends Activity {
 		}
 		textAge.setDrawingCacheEnabled(true);
 		Bitmap bitmap = Bitmap.createBitmap(textAge.getDrawingCache());
+		textAge.setVisibility(View.INVISIBLE);
 		textAge.destroyDrawingCache();
 		return bitmap;
 	}
@@ -194,61 +208,76 @@ public class FaceDetectActivity extends Activity {
 			// find out all faces
 			JSONArray faceArray = rst.getJSONArray("face");
 			final int faceCount = faceArray.length();
-			textState.setText("Finished, " + faceCount + " faces.");
-			for (int i = 0; i < faceCount; ++i) {
-				float x, y, w, h;
-				// 获取人脸中心
-				JSONObject faceObject = faceArray.getJSONObject(i);
-				JSONObject positionObject = faceObject
-						.getJSONObject("position");
-				x = (float) positionObject.getJSONObject("center").getDouble(
-						"x");
-				y = (float) faceObject.getJSONObject("position")
-						.getJSONObject("center").getDouble("y");
+			btnShare.setVisibility(View.VISIBLE);
+			detectProgress.setVisibility(View.GONE);
+			if (faceCount > 0) {
+				textHint.setText("识别出" + faceCount + "个脸部，分享给好友");
+				imageHintArrow1.setVisibility(View.INVISIBLE);
+				imageHintArrow2.setVisibility(View.INVISIBLE);
+				imageHintArrow3.setVisibility(View.VISIBLE);
+				textAge.setVisibility(View.INVISIBLE);
+				for (int i = 0; i < faceCount; ++i) {
+					float x, y, w, h;
+					// 获取人脸中心
+					JSONObject faceObject = faceArray.getJSONObject(i);
+					JSONObject positionObject = faceObject
+							.getJSONObject("position");
+					x = (float) positionObject.getJSONObject("center")
+							.getDouble("x");
+					y = (float) faceObject.getJSONObject("position")
+							.getJSONObject("center").getDouble("y");
 
-				// 获取人脸大小
-				w = (float) positionObject.getDouble("width");
-				h = (float) positionObject.getDouble("height");
+					// 获取人脸大小
+					w = (float) positionObject.getDouble("width");
+					h = (float) positionObject.getDouble("height");
 
-				// 绘制人脸方框
-				x = x / 100 * bitmapPhoto.getWidth();
-				y = y / 100 * bitmapPhoto.getHeight();
-				w = w / 100 * bitmapPhoto.getWidth();
-				h = h / 100 * bitmapPhoto.getHeight();
-				canvas.drawLine(x - w / 2, y - h / 2, x - w / 2, y + h / 2,
-						paint);
-				canvas.drawLine(x - w / 2, y - h / 2, x + w / 2, y - h / 2,
-						paint);
-				canvas.drawLine(x + w / 2, y - h / 2, x + w / 2, y + h / 2,
-						paint);
-				canvas.drawLine(x - w / 2, y + h / 2, x + w / 2, y + h / 2,
-						paint);
+					// 绘制人脸方框
+					x = x / 100 * bitmapPhoto.getWidth();
+					y = y / 100 * bitmapPhoto.getHeight();
+					w = w / 100 * bitmapPhoto.getWidth();
+					h = h / 100 * bitmapPhoto.getHeight();
+					canvas.drawLine(x - w / 2, y - h / 2, x - w / 2, y + h / 2,
+							paint);
+					canvas.drawLine(x - w / 2, y - h / 2, x + w / 2, y - h / 2,
+							paint);
+					canvas.drawLine(x + w / 2, y - h / 2, x + w / 2, y + h / 2,
+							paint);
+					canvas.drawLine(x - w / 2, y + h / 2, x + w / 2, y + h / 2,
+							paint);
 
-				// 性别和年龄
-				int age = faceObject.getJSONObject("attribute")
-						.getJSONObject("age").getInt("value");
-				int range = faceObject.getJSONObject("attribute")
-						.getJSONObject("age").getInt("range");
-				String gendorStr = faceObject.getJSONObject("attribute")
-						.getJSONObject("gender").getString("value"); // Male-Female
-				Bitmap ageBitmap = getGendorBitmap(age + range,
-						"Male".equals(gendorStr));
+					// 性别和年龄
+					int age = faceObject.getJSONObject("attribute")
+							.getJSONObject("age").getInt("value");
+					int range = faceObject.getJSONObject("attribute")
+							.getJSONObject("age").getInt("range");
+					String gendorStr = faceObject.getJSONObject("attribute")
+							.getJSONObject("gender").getString("value"); // Male-Female
+					Bitmap ageBitmap = getGendorBitmap(age + range,
+							"Male".equals(gendorStr));
 
-				int ageWidth = ageBitmap.getWidth();
-				int ageHeight = ageBitmap.getHeight();
+					int ageWidth = ageBitmap.getWidth();
+					int ageHeight = ageBitmap.getHeight();
 
-				if (bitmap.getWidth() < imagePhoto.getWidth()
-						&& bitmap.getHeight() < imagePhoto.getHeight()) {
-					float ratio = Math.max(bitmap.getWidth() * 1.0f
-							/ imagePhoto.getWidth(), bitmap.getHeight() * 1.0f
-							/ imagePhoto.getHeight());
-					ageBitmap = Bitmap.createScaledBitmap(ageBitmap,
-							(int) (ageWidth * ratio),
-							(int) (ageHeight * ratio), false);
+					if (bitmap.getWidth() < imagePhoto.getWidth()
+							&& bitmap.getHeight() < imagePhoto.getHeight()) {
+						float ratio = Math.max(bitmap.getWidth() * 1.0f
+								/ imagePhoto.getWidth(), bitmap.getHeight()
+								* 1.0f / imagePhoto.getHeight());
+						ageBitmap = Bitmap.createScaledBitmap(ageBitmap,
+								(int) (ageWidth * ratio),
+								(int) (ageHeight * ratio), false);
+					}
+
+					canvas.drawBitmap(ageBitmap, x - ageBitmap.getWidth() / 2,
+							y - h / 2 - ageBitmap.getHeight(), null);
 				}
-
-				canvas.drawBitmap(ageBitmap, x - ageBitmap.getWidth() / 2, y
-						- h / 2 - ageBitmap.getHeight(), null);
+			} else {
+				textHint.setText("未识别到脸部，换张照片试试");
+				imageHintArrow1.setVisibility(View.VISIBLE);
+				imageHintArrow2.setVisibility(View.INVISIBLE);
+				imageHintArrow3.setVisibility(View.INVISIBLE);
+				btnDetect.setVisibility(View.INVISIBLE);
+				btnShare.setVisibility(View.INVISIBLE);
 			}
 
 			// save new image
@@ -295,7 +324,11 @@ public class FaceDetectActivity extends Activity {
 				textState.setText("Clik Detect. ==>");
 
 				imagePhoto.setImageBitmap(bitmapPhoto);
-				buttonDetect.setVisibility(View.VISIBLE);
+				btnDetect.setVisibility(View.VISIBLE);
+				textHint.setText("开始人脸年龄识别");
+				imageHintArrow1.setVisibility(View.INVISIBLE);
+				imageHintArrow2.setVisibility(View.VISIBLE);
+				imageHintArrow3.setVisibility(View.INVISIBLE);
 			} else {
 				Log.d(TAG, "idButSelPic Photopicker canceled");
 			}
