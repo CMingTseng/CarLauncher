eefbf8d43dbbb839742a4dc3a1c5b7c539a5daaa
zhoumushui <zhoumushui@gmail.com>
2015-06-19 17:05:17 +0800

    [ZMS]Add power state change reveiver.

---
+++ src/com/tchip/carlauncher/model/PowerStateChangeReceiver.java
@@ -0,0 +1,52 @@
+package com.tchip.carlauncher.model;
+
+import com.tchip.carlauncher.service.SpeakService;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.BatteryManager;
+import android.widget.Toast;
+
+public class PowerStateChangeReceiver extends BroadcastReceiver {
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		// int status = intent.getIntExtra(BatteryManager.EXTRA_STATUS, -1);
+		// boolean isCharging = status == BatteryManager.BATTERY_STATUS_CHARGING
+		// || status == BatteryManager.BATTERY_STATUS_FULL;
+		//
+		// int chargePlug = intent.getIntExtra(BatteryManager.EXTRA_PLUGGED,
+		// -1);
+		// boolean usbCharge = chargePlug == BatteryManager.BATTERY_PLUGGED_USB;
+		// boolean acCharge = chargePlug == BatteryManager.BATTERY_PLUGGED_AC;
+		// Toast.makeText(
+		// context,
+		// "isCharging:" + isCharging + "-usbCharge:" + usbCharge
+		// + "-acCharge:" + acCharge, Toast.LENGTH_SHORT).show();
+
+		// //当前剩余电量
+		// int level = batteryStatus.getIntExtra(BatteryManager.EXTRA_LEVEL,
+		// -1);
+		// //电量最大值
+		// int scale = batteryStatus.getIntExtra(BatteryManager.EXTRA_SCALE,
+		// -1);
+		// //电量百分比
+		// float batteryPct = level / (float)scale;
+
+		if ("android.intent.action.ACTION_POWER_CONNECTED".equals(intent
+				.getAction())) {
+			// String strHintConnect = "";
+			// Toast.makeText(context, "CONNECTED", Toast.LENGTH_SHORT).show();
+		} else if ("android.intent.action.ACTION_POWER_DISCONNECTED"
+				.equals(intent.getAction())) {
+			String strHintDisconnect = "已断开电源，10秒后自动保存数据。";
+			Toast.makeText(context, strHintDisconnect, Toast.LENGTH_SHORT).show();
+			Intent intentSpeak = new Intent(context, SpeakService.class);
+			intentSpeak.putExtra("content", strHintDisconnect);
+			context.startService(intentSpeak);
+		}
+	}
+	
+
+}
