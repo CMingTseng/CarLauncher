7e40d9261ddc7b8d549ab094f57890f453f84d81
zhoumushui <zhoumushui@gmail.com>
2015-10-13 21:05:32 +0800

    [ZMS]Tmp commit.

---
+++ src/com/tchip/carlauncher/service/SleepOnOffService.java
@@ -0,0 +1,179 @@
+package com.tchip.carlauncher.service;
+
+import com.tchip.carlauncher.Constant;
+import com.tchip.carlauncher.MyApplication;
+import com.tchip.carlauncher.ui.activity.MainActivity;
+import com.tchip.carlauncher.util.MyLog;
+import com.tchip.carlauncher.util.SettingUtil;
+
+import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.media.AudioManager;
+import android.os.IBinder;
+import android.os.PowerManager;
+import android.provider.Settings;
+import android.widget.Toast;
+
+public class SleepOnOffService extends Service {
+	private Context context;
+	private SharedPreferences sharedPreferences;
+	private Editor editor;
+	private PowerManager powerManager;
+
+	@Override
+	public IBinder onBind(Intent intent) {
+		return null;
+	}
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+
+		context = getApplicationContext();
+
+		sharedPreferences = getSharedPreferences(
+				Constant.SHARED_PREFERENCES_NAME, Context.MODE_PRIVATE);
+		editor = sharedPreferences.edit();
+
+		powerManager = (PowerManager) context
+				.getSystemService(Context.POWER_SERVICE);
+
+		// 动态注册监听函数
+		sleepOnOffReceiver = new SleepOnOffReceiver();
+		IntentFilter filter = new IntentFilter();
+		filter.addAction("com.tchip.SLEEP_ON");
+		filter.addAction("com.tchip.SLEEP_OFF");
+		filter.addAction("com.tchip.GSENSOR_CRASH");
+		registerReceiver(sleepOnOffReceiver, filter);
+
+	}
+
+	private SleepOnOffReceiver sleepOnOffReceiver;
+
+	public class SleepOnOffReceiver extends BroadcastReceiver {
+
+		@Override
+		public void onReceive(Context context, Intent intent) {
+
+			String action = intent.getAction();
+			MyLog.v("[SleepOnOffReceiver]action:" + action);
+			if (action.equals("com.tchip.SLEEP_ON")) {
+				startSpeak("下"); // TEST
+
+				try {
+
+					// 进入低功耗待机
+					MyApplication.isSleeping = true;
+
+					// 熄灭屏幕,判断当前屏幕是否关闭
+					boolean isScreenOn = powerManager.isScreenOn();
+					if (!isScreenOn) {
+					} else {
+						context.sendBroadcast(new Intent("com.tchip.powerKey")
+								.putExtra("value", "power"));
+					}
+
+					// 打开飞行模式
+					context.sendBroadcast(new Intent("com.tchip.AIRPLANE_ON"));
+
+					// 关闭GPS
+					context.sendBroadcast(new Intent(
+							"tchip.intent.action.ACTION_GPS_OFF"));
+
+					// 关闭FM发射，并保存休眠前状态
+					boolean fmStateBeforeSleep = SettingUtil
+							.isFmTransmitOn(context);
+					editor.putBoolean("fmStateBeforeSleep", fmStateBeforeSleep);
+					editor.commit();
+					if (fmStateBeforeSleep) {
+						MyLog.v("[SleepReceiver]Sleep:close FM");
+						Settings.System.putString(context.getContentResolver(),
+								Constant.FMTransmit.SETTING_ENABLE, "0");
+						SettingUtil.SaveFileToNode(SettingUtil.nodeFmEnable,
+								"0");
+					}
+
+				} catch (Exception e) {
+					MyLog.e("[SleepReceiver]Error when run com.tchip.SLEEP_ON");
+				}
+			} else if (action.equals("com.tchip.SLEEP_OFF")) {
+				try {
+					startSpeak("上"); // TEST
+
+					// 取消低功耗待机
+					MyApplication.isSleeping = false;
+
+					// MainActivity,BackThread的Handler启动AutoThread,启动录像和服务
+					MyApplication.shouldWakeRecord = true;
+
+					// 发送Home键，回到主界面
+//					context.sendBroadcast(new Intent("com.tchip.powerKey")
+//							.putExtra("value", "home"));
+
+					// 关闭飞行模式
+					context.sendBroadcast(new Intent("com.tchip.AIRPLANE_OFF"));
+
+					// 打开GPS
+					context.sendBroadcast(new Intent(
+							"tchip.intent.action.ACTION_GPS_ON"));
+
+					// 重置FM发射状态
+					boolean fmStateBeforeSleep = sharedPreferences.getBoolean(
+							"fmStateBeforeSleep", false);
+					if (fmStateBeforeSleep) {
+						MyLog.v("[SleepReceiver]WakeUp:open FM Transmit");
+						Settings.System.putString(context.getContentResolver(),
+								Constant.FMTransmit.SETTING_ENABLE, "1");
+						SettingUtil.SaveFileToNode(SettingUtil.nodeFmEnable,
+								"1");
+					}
+				} catch (Exception e) {
+					MyLog.e("[SleepReceiver]Error when run com.tchip.SLEEP_OFF");
+				}
+
+			} else if (action.equals("com.tchip.GSENSOR_CRASH")) {
+				// 休眠时碰撞侦测，接收到碰撞，亮屏录制一段视频，然后休眠
+				MyLog.v("[GSENSOR_CRASH]Before State->shouldCrashRecord:"
+						+ MyApplication.shouldCrashRecord
+						+ ",shouldStopWhenCrashVideoSave:"
+						+ MyApplication.shouldStopWhenCrashVideoSave);
+
+				// 发送Home键，回到主界面
+//				context.sendBroadcast(new Intent("com.tchip.powerKey")
+//						.putExtra("value", "home"));
+
+				// 点亮屏幕
+				// SettingUtil.lightScreen(context);
+
+				if (MyApplication.shouldCrashRecord
+						|| MyApplication.shouldStopWhenCrashVideoSave) {
+				} else {
+					MyApplication.shouldCrashRecord = true;
+					MyApplication.shouldStopWhenCrashVideoSave = true;
+				}
+			}
+		}
+
+	}
+
+	private void startSpeak(String content) {
+		Intent intent = new Intent(getApplicationContext(), SpeakService.class);
+		intent.putExtra("content", content);
+		startService(intent);
+	}
+
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+
+		if (sleepOnOffReceiver != null) {
+			unregisterReceiver(sleepOnOffReceiver);
+		}
+	}
+
+}
--- src/com/tchip/carlauncher/ui/activity/MainActivity.java
+++ src/com/tchip/carlauncher/ui/activity/MainActivity.java
@@ -13,6 +13,7 @@ import com.tchip.carlauncher.model.DriveVideoDbHelper;
 import com.tchip.carlauncher.model.Typefaces;
 import com.tchip.carlauncher.service.RouteRecordService;
 import com.tchip.carlauncher.service.SensorWatchService;
+import com.tchip.carlauncher.service.SleepOnOffService;
 import com.tchip.carlauncher.service.SpeakService;
 import com.tchip.carlauncher.util.AudioPlayUtil;
 import com.tchip.carlauncher.util.ClickUtil;
@@ -35,7 +36,6 @@ import android.content.IntentFilter;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
 import android.hardware.Camera;
-import android.media.AudioManager;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.Uri;
@@ -44,12 +44,10 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.os.PowerManager;
-import android.provider.Settings;
 import android.telephony.PhoneStateListener;
 import android.telephony.SignalStrength;
 import android.telephony.TelephonyManager;
 import android.view.KeyEvent;
-import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
@@ -156,150 +154,9 @@ public class MainActivity extends Activity implements TachographCallback,
 		// 初始化节点状态
 		initialNodeState();
 
-		// 动态注册监听函数
-		sleepOnOffReceiver = new SleepOnOffReceiver();
-		IntentFilter filter = new IntentFilter();
-		filter.addAction("com.tchip.SLEEP_ON");
-		filter.addAction("com.tchip.SLEEP_OFF");
-		filter.addAction("com.tchip.GSENSOR_CRASH");
-		registerReceiver(sleepOnOffReceiver, filter);
-	}
-
-	private SleepOnOffReceiver sleepOnOffReceiver;
-	private AudioManager audioManager;
-
-	public class SleepOnOffReceiver extends BroadcastReceiver {
-
-		@Override
-		public void onReceive(Context context, Intent intent) {
-			audioManager = (AudioManager) context
-					.getSystemService(Context.AUDIO_SERVICE);
-
-			String action = intent.getAction();
-			MyLog.v("[SleepOnOffReceiver]action:" + action);
-			if (action.equals("com.tchip.SLEEP_ON")) {
-				try {
-					// 进入低功耗待机
-					MyApplication.isSleeping = true;
-					// 打开飞行模式
-					context.sendBroadcast(new Intent("com.tchip.AIRPLANE_ON"));
-
-					// 熄灭屏幕,判断当前屏幕是否关闭
-					PowerManager pm = (PowerManager) context
-							.getSystemService(Context.POWER_SERVICE);
-					boolean isScreenOn = pm.isScreenOn();
-					if (!isScreenOn) {
-					} else {
-						context.sendBroadcast(new Intent("com.tchip.powerKey")
-								.putExtra("value", "power"));
-					}
-
-					// 关闭GPS
-					context.sendBroadcast(new Intent(
-							"tchip.intent.action.ACTION_GPS_OFF"));
-
-					// 关闭FM发射，并保存休眠前状态
-					boolean fmStateBeforeSleep = SettingUtil
-							.isFmTransmitOn(context);
-					editor.putBoolean("fmStateBeforeSleep", fmStateBeforeSleep);
-					editor.commit();
-					if (fmStateBeforeSleep) {
-						MyLog.v("[SleepReceiver]Sleep:close FM");
-						Settings.System.putString(context.getContentResolver(),
-								Constant.FMTransmit.SETTING_ENABLE, "0");
-						SettingUtil.SaveFileToNode(SettingUtil.nodeFmEnable,
-								"0");
-					}
-
-					// 静音
-					// 媒体声音
-					if (Constant.Module.muteWhenSleep) {
-						int volumeMusic = audioManager
-								.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
-						int volumeRing = audioManager
-								.getStreamVolume(AudioManager.STREAM_RING);
-						editor.putInt("volumeMusic", volumeMusic);
-						editor.putInt("volumeRing", volumeRing);
-						editor.commit();
-					}
-				} catch (Exception e) {
-					MyLog.e("[SleepReceiver]Error when run com.tchip.SLEEP_ON");
-				} finally {
-					// 发送Home键，回到主界面
-					context.sendBroadcast(new Intent("com.tchip.powerKey")
-							.putExtra("value", "home"));
-				}
-			} else if (action.equals("com.tchip.SLEEP_OFF")) {
-				try {
-					// 取消低功耗待机
-					MyApplication.isSleeping = false;
-
-					// 发送Home键，回到主界面
-					context.sendBroadcast(new Intent("com.tchip.powerKey")
-							.putExtra("value", "home"));
-
-					// 点亮屏幕
-					SettingUtil.lightScreen(context);
-
-					// 关闭飞行模式
-					context.sendBroadcast(new Intent("com.tchip.AIRPLANE_OFF"));
-
-					// 打开GPS
-					context.sendBroadcast(new Intent(
-							"tchip.intent.action.ACTION_GPS_ON"));
-
-					// 重置FM发射状态
-					boolean fmStateBeforeSleep = sharedPreferences.getBoolean(
-							"fmStateBeforeSleep", false);
-					if (fmStateBeforeSleep) {
-						MyLog.v("[SleepReceiver]WakeUp:open FM Transmit");
-						Settings.System.putString(context.getContentResolver(),
-								Constant.FMTransmit.SETTING_ENABLE, "1");
-						SettingUtil.SaveFileToNode(SettingUtil.nodeFmEnable,
-								"1");
-					}
-
-					// 恢复音量设置
-					if (Constant.Module.muteWhenSleep) {
-						int volumeMusic = sharedPreferences.getInt(
-								"volumeMusic", 8);
-						int volumeRing = sharedPreferences.getInt("volumeRing",
-								8);
-						audioManager.setStreamVolume(AudioManager.STREAM_MUSIC,
-								volumeMusic, 0);
-						audioManager.setStreamVolume(AudioManager.STREAM_RING,
-								volumeRing, 0);
-					}
-
-					// MainActivity,BackThread的Handler启动AutoThread,启动录像和服务
-					MyApplication.shouldWakeRecord = true;
-
-				} catch (Exception e) {
-					MyLog.e("[SleepReceiver]Error when run com.tchip.SLEEP_OFF");
-				}
-			} else if (action.equals("com.tchip.GSENSOR_CRASH")) {
-				// 休眠时碰撞侦测，接收到碰撞，亮屏录制一段视频，然后休眠
-				MyLog.v("[GSENSOR_CRASH]Before State->shouldCrashRecord:"
-						+ MyApplication.shouldCrashRecord
-						+ ",shouldStopWhenCrashVideoSave:"
-						+ MyApplication.shouldStopWhenCrashVideoSave);
-
-				// 发送Home键，回到主界面
-				context.sendBroadcast(new Intent("com.tchip.powerKey")
-						.putExtra("value", "home"));
-
-				// 点亮屏幕
-				SettingUtil.lightScreen(context);
-
-				if (MyApplication.shouldCrashRecord
-						|| MyApplication.shouldStopWhenCrashVideoSave) {
-				} else {
-					MyApplication.shouldCrashRecord = true;
-					MyApplication.shouldStopWhenCrashVideoSave = true;
-				}
-			}
-		}
-
+		Intent intentSleepOnOff = new Intent(MainActivity.this,
+				SleepOnOffService.class);
+		startService(intentSleepOnOff);
 	}
 
 	/**
@@ -343,7 +200,7 @@ public class MainActivity extends Activity implements TachographCallback,
 					autoHandler.sendMessage(message);
 				}
 				// 启动服务
-				Thread.sleep(1000);
+				// Thread.sleep(1000);
 				initialService();
 
 			} catch (InterruptedException e) {
@@ -455,10 +312,10 @@ public class MainActivity extends Activity implements TachographCallback,
 				if (!MyApplication.isMainForeground) {
 					// 录像需切换到预览界面且点亮屏幕，否则无法录像
 					// 发送Home键，回到主界面
-					sendBroadcast(new Intent("com.tchip.powerKey").putExtra(
-							"value", "home"));
+//					sendBroadcast(new Intent("com.tchip.powerKey").putExtra(
+//							"value", "home"));
 					// 点亮屏幕
-					SettingUtil.lightScreen(getApplicationContext());
+					//SettingUtil.lightScreen(getApplicationContext());
 				}
 				// 开始录像
 				new Thread(new StartRecordThread()).start();
@@ -1045,8 +902,10 @@ public class MainActivity extends Activity implements TachographCallback,
 						Toast.LENGTH_SHORT).show();
 				MyLog.e("Record Stop:sleep on.");
 				startSpeak(strSleepOn);
-				audioRecordDialog.showErrorDialog(strSleepOn);
-				new Thread(new dismissDialogThread()).start();
+
+				// audioRecordDialog.showErrorDialog(strSleepOn);
+				// new Thread(new dismissDialogThread()).start();
+
 				break;
 
 			default:
@@ -1408,15 +1267,16 @@ public class MainActivity extends Activity implements TachographCallback,
 		try {
 			if (mRecordState == Constant.Record.STATE_RECORD_STOPPED) {
 				if (MyApplication.isSleeping) {
-					startSpeak("正在休眠，无法录像");
+					startSpeak(getResources().getString(
+							R.string.stop_record_sleeping));
 				} else {
 					if (!MyApplication.isMainForeground) {
 						// 录像需切换到预览界面且点亮屏幕，否则无法录像
 						// 发送Home键，回到主界面
-						sendBroadcast(new Intent("com.tchip.powerKey")
-								.putExtra("value", "home"));
+//						sendBroadcast(new Intent("com.tchip.powerKey")
+//								.putExtra("value", "home"));
 						// 点亮屏幕
-						SettingUtil.lightScreen(getApplicationContext());
+						//SettingUtil.lightScreen(getApplicationContext());
 					}
 					// 开始录像
 					new Thread(new StartRecordThread()).start();
@@ -1611,10 +1471,6 @@ public class MainActivity extends Activity implements TachographCallback,
 			unregisterReceiver(wifiIntentReceiver);
 		}
 
-		if (sleepOnOffReceiver != null) {
-			unregisterReceiver(sleepOnOffReceiver);
-		}
-
 		super.onDestroy();
 	}
 
@@ -1801,6 +1657,7 @@ public class MainActivity extends Activity implements TachographCallback,
 				sdcardPath = Constant.Path.SDCARD_2 + File.separator;// "/storage/sdcard2/";
 			}
 			// sharedPreferences.getString("sdcardPath","/mnt/sdcard2");
+
 			float sdFree = StorageUtil.getSDAvailableSize(sdcardPath);
 			float sdTotal = StorageUtil.getSDTotalSize(sdcardPath);
 			int intSdFree = (int) sdFree;
@@ -2317,8 +2174,9 @@ public class MainActivity extends Activity implements TachographCallback,
 
 		if (!MyApplication.isVideoReording) {
 			// 需要在当前视频存储到数据库之后，且当前未录像时再进行;当行车视频较多时，该操作比较耗时
-			CheckErrorFile();
+			// CheckErrorFile(); // TEST
 		}
+
 	}
 
 	public void setup() {
