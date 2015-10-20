51d23997381eabf6ad6630a6ba46dadb1c855eb5
zhoumushui <zhoumushui@gmail.com>
2015-10-20 11:46:50 +0800

    [ZMS]Change style of volume setting activity to dialog.

--- src/com/tchip/carlauncher/ui/activity/SettingSystemVolumeActivity.java
+++ src/com/tchip/carlauncher/ui/activity/SettingSystemVolumeActivity.java
@@ -1,22 +1,15 @@
 package com.tchip.carlauncher.ui.activity;
 
-import com.tchip.carlauncher.Constant;
 import com.tchip.carlauncher.R;
-import com.tchip.carlauncher.model.Typefaces;
-import com.tchip.carlauncher.view.NumberSeekBar;
 
 import android.app.Activity;
 import android.content.Context;
-import android.graphics.Color;
 import android.media.AudioManager;
 import android.os.Bundle;
-import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.SeekBar;
 import android.widget.SeekBar.OnSeekBarChangeListener;
-import android.widget.TextClock;
-import android.widget.RelativeLayout;
 
 public class SettingSystemVolumeActivity extends Activity {
 
@@ -29,15 +22,6 @@ public class SettingSystemVolumeActivity extends Activity {
 		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
 				WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		setContentView(R.layout.activity_setting_system_volume);
-
-		// 返回
-		RelativeLayout layoutToSettingFromBright = (RelativeLayout) findViewById(R.id.layoutToSettingFromBright);
-		layoutToSettingFromBright.setOnClickListener(new MyOnClickListener());
-
-		// 时钟
-		TextClock textClock = (TextClock) findViewById(R.id.textClock);
-		textClock.setTypeface(Typefaces.get(this, Constant.Path.FONT
-				+ "Font-Helvetica-Neue-LT-Pro.otf"));
 
 		audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
 
@@ -100,21 +84,6 @@ public class SettingSystemVolumeActivity extends Activity {
 
 			}
 		});
-	}
-
-	class MyOnClickListener implements View.OnClickListener {
-
-		@Override
-		public void onClick(View v) {
-			switch (v.getId()) {
-			case R.id.layoutToSettingFromBright:
-				finish();
-				break;
-
-			default:
-				break;
-			}
-		}
 	}
 
 }
